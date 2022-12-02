daf1cc9064a630b7decb1407726766c7351f0a72
Aditya K <adityakaklij11@gmail.com>
2022-12-03 04:07:59 +0530

    deploy token update

--- src/Components/Analytics.js
+++ src/Components/Analytics.js
@@ -4,6 +4,14 @@ function Analytics() {
   return (
     <div>
         <h1>Analytics</h1>
+
+        <h3>Total transactions:- </h3>
+        <br />
+
+        <h3>Total turnOver</h3>
+        <br />
+
+        <h3>Chain tx.</h3>
     </div>
   )
 }
--- src/Components/Deploy.js
+++ src/Components/Deploy.js
@@ -5,13 +5,16 @@ import { contractABI, contractByteCode, ETHContractAddress, polygonContractAddre
 function Deploy() {
 
     const [name,setName]= useState("Payment Gateway")
+    const [value, setValue] = useState();
+    const m = []
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
 
     const deployContract = async () => {
         const factory = new ethers.ContractFactory(contractABI, contractByteCode, signer)
-        const contract = await factory.deploy();
+        const contract = await factory.deploy(["0x5a1899faff22a2b3ea0294d86cd1be6269931ef1","0x0F71B2a7898EE371b8D1fDc9352dC9cbBC18294e"]);
+        // const contract = await factory.deploy();
         console.log("address- ", contract.address)
         window.alert(`contract depolyed at ${contract.address}`)
     }
@@ -19,6 +22,21 @@ function Deploy() {
     function GetName(e){
         setName(e.target.value)
     }
+
+    function getDropDown() {
+        console.log(m)
+        
+    }
+
+    const handleChange = (e) => {
+        m.push(e.target.value)
+        // setValue.push[e.target.value];
+    };
+
+    const getInitialState = () => {
+        const value = "ETH";
+        return value;
+    };
 
     //  ########## EPNS ########
     // const getData = async ()=> {
@@ -61,6 +79,19 @@ function Deploy() {
         <br />
         <br />
 
+        <label for="cars">Select Tokens:</label>
+
+        <select onChange={handleChange} name="make" className="selectpicker" multiple data-live-search="true">
+            <option data-count="2" value="USDT">USDT</option>
+            <option data-count="23" value="ETH">ETH</option>
+            <option data-count="433" value="MATIC">MATIC</option>
+            <option data-count="45" value="SHM">SHM</option>
+            <option data-count="476" value="fETH">fETH</option>
+            <option data-count="78" value="Other">Other</option>
+            
+        </select>
+
+
         <h4>Contract-Name</h4>
         <input type="text" placeholder='Contract Name' onChange={GetName}/>
 
@@ -68,8 +99,9 @@ function Deploy() {
         <br />
 
         <button onClick={deployContract}>Deploy Contract</button>
+        <button onClick={getDropDown}>getDropDown</button>
     </>
-  )
+  );
 }
 
 export default Deploy
